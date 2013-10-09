122811dbdcd333880d7e0d354b2360d126506e12
Amy Greene <amygreene@gmail.com>
2013-10-09 11:40:04 -0700

    updating wp_register url to network_home

--- functions.php
+++ functions.php
@@ -57,4 +57,17 @@ function remove_default_headers() {
  
 add_action( 'after_setup_theme', 'remove_default_headers', 11 );
 
+//replace url in wp_register() to point to network_home register page
+add_action( 'register' , 'register_replacement' );
+function register_replacement( $link ){
+	if ( ! is_user_logged_in() ) {
+		if ( get_option('users_can_register') )
+			$link = $before . '<a href="' . network_home_url('register', 'login') . '">' . __('Register') . '</a>' . $after;
+		else
+			$link = '';
+	} else {
+		$link = $before . '<a href="' . admin_url() . '">' . __('Site Admin') . '</a>' . $after;
+	}
+	return $link;
+}
 ?>
